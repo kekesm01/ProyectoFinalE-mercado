9dcc81f94ad3773762cc2d5fe4c02f16dd888732
kekesm01 <kevinsiragusamiguez@gmail.com>
2025-08-18 16:33:16 -0300

    armado-y-funcionalidad-login

---
+++ js/login.js
@@ -0,0 +1,15 @@
+let botonIngresar = document.getElementById("boton-ingresar");
+
+botonIngresar.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let usuario = document.getElementById("usuario").value;
+    let contrasena = document.getElementById("contrasena").value;
+
+    if (usuario !== "" && contrasena !== "") {
+        window.location.href = "index.html";
+    } else {
+        alert("Por favor, completa todos los campos");
+    }
+});
+
